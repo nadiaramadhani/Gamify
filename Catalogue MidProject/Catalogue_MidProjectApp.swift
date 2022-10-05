615057bd230f3b695c637a95e96dc35e398e38e4
Nadia Ramadhani <103446529+nadiaramadhani@users.noreply.github.com>
2022-10-05 14:51:37 +0700

    Initial Commit

---
+++ Catalogue MidProject/Catalogue_MidProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catalogue_MidProjectApp.swift
+//  Catalogue MidProject
+//
+//  Created by Local Administrator on 05/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Catalogue_MidProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
